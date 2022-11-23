2f9d482209184a6661805c78a9de91459d0f1909
youssefa123 <yabdelhady905@gmail.com>
2022-11-23 17:48:15 -0500

    Add files via upload
    Import FIles

---
+++ _site/assets/js/categories.js
@@ -0,0 +1,32 @@
+const categories = { fiction: [{ url: `/posts/the-purpose-of-education/`, date: `23 Dec 0199`, title: `Resume`},],jekyll: [{ url: `/posts/jekyll-markdown/`, date: `04 Jan 2022`, title: `Lessons`},{ url: `/posts/the-purpose-of-education/`, date: `23 Dec 0199`, title: `Resume`},], }
+
+console.log(categories);
+
+window.onload = function () {
+  document.querySelectorAll(".article-category").forEach((category) => {
+    category.addEventListener("click", function (e) {
+      const posts = categories[e.target.innerText.replace(" ","_")];
+      console.log(posts);
+      let html = ``
+      posts.forEach(post=>{
+        html += `
+        <a class="modal-article" href="${post.url}">
+          <h4>${post.title}</h4>
+          <small class="modal-article-date">${post.date}</small>
+        </a>
+        `
+      })
+      document.querySelector("#category-modal-title").innerText = e.target.innerText;
+      document.querySelector("#category-modal-content").innerHTML = html;
+      document.querySelector("#category-modal-bg").classList.toggle("open");
+      document.querySelector("#category-modal").classList.toggle("open");
+    });
+  });
+
+  document.querySelector("#category-modal-bg").addEventListener("click", function(){
+    document.querySelector("#category-modal-title").innerText = "";
+    document.querySelector("#category-modal-content").innerHTML = "";
+    document.querySelector("#category-modal-bg").classList.toggle("open");
+    document.querySelector("#category-modal").classList.toggle("open");
+  })
+};
